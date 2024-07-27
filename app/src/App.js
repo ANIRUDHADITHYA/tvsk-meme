1c7c376b08926311998a75b5cfd590fe0eddc09d
ANIRUDHADITHYA <anirudhadithya.b.s@gmail.com>
2024-07-27 14:53:23 +0530

    updated images

--- app/src/App.js
+++ app/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   const [clicks, setClicks] = useState([]);
-  const [shrinkCoin, setShrinkCoin] = useState(null); // state to handle shrinking coin animation
+  const [shrinkCoin, setShrinkCoin] = useState(null);
   const [khBalance, setKhBalance] = useState(0);
   const [dtBalance, setDtBalance] = useState(0);
 
@@ -13,10 +13,24 @@ function App() {
     audio.play();
   };
 
+  const formatBalance = (balance) => {
+    if (balance >= 1e12) {
+      return `${(balance / 1e12).toFixed(1)} T`;
+    } else if (balance >= 1e9) {
+      return `${(balance / 1e9).toFixed(1)} B`;
+    } else if (balance >= 1e6) {
+      return `${(balance / 1e6).toFixed(1)} M`;
+    } else if (balance >= 1e3) {
+      return `${(balance / 1e3).toFixed(1)} K`;
+    } else {
+      return balance.toString();
+    }
+  };
+
   useEffect(() => {
     const fetchBalances = async () => {
       try {
-        const response = await axios.get(`http://localhost:3001/api/get-balances`);
+        const response = await axios.get(`https://api.battleofpresident.fun/api/get-balances`);
         setKhBalance(response.data.khBalance);
         setDtBalance(response.data.dtBalance);
       } catch (error) {
@@ -28,26 +42,19 @@ function App() {
   }, []);
 
   const handleCoinClick = async (coinType, e) => {
-    // Play beep sound
     playBeepSound();
-
-    // Get the click position relative to the container
     const rect = e.currentTarget.getBoundingClientRect();
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
 
-    // Add click animation
     setClicks((prevClicks) => [...prevClicks, { id: Date.now(), x, y }]);
-
-    // Apply shrinking animation
     setShrinkCoin(coinType);
     setTimeout(() => {
-      setShrinkCoin(null); // Remove the shrinking animation class after 300ms
+      setShrinkCoin(null);
     }, 100);
 
-    // Call API to update balance
     try {
-      const response = await axios.post(`http://localhost:3001/api/update-balance`, {
+      const response = await axios.post(`https://api.battleofpresident.fun/api/update-balance`, {
         coinType,
       });
       if (coinType === 'kh') {
@@ -59,7 +66,6 @@ function App() {
       console.error('Error updating balance:', error);
     }
 
-    // Remove the click animation after it finishes
     setTimeout(() => {
       setClicks((prevClicks) => prevClicks.filter((click) => click.id !== Date.now()));
     }, 1000);
@@ -76,12 +82,12 @@ function App() {
       </nav>
       <div className='coin-wrapper'>
         <img
-          src='/kh.png'
+          src='/kh1.png'
           className={`kh-coin ${shrinkCoin === 'kh' ? 'shrink' : ''}`}
           onClick={(e) => handleCoinClick('kh', e)}
         ></img>
         <img
-          src='/dt.png'
+          src='/dt1.png'
           className={`dt-coin ${shrinkCoin === 'dt' ? 'shrink' : ''}`}
           onClick={(e) => handleCoinClick('dt', e)}
         ></img>
@@ -91,13 +97,13 @@ function App() {
       </div>
       <div className='poll-results'>
         <div className='result-inner-container kh'>
-          <img src='/khr.png'></img>
-          <h1>{khBalance.toLocaleString()}</h1>
+          <img src='/khr.png' alt='KH' />
+          <h1>{formatBalance(khBalance)}</h1>
         </div>
-        <img className='vs' src='/tt.gif'></img>
+        <img className='vs' src='/tt.gif' alt='vs' />
         <div className='result-inner-container dt'>
-          <img src='/dtr.png'></img>
-          <h1>{dtBalance.toLocaleString()}</h1>
+          <img src='/dtr.png' alt='DT' />
+          <h1>{formatBalance(dtBalance)}</h1>
         </div>
       </div>
       {clicks.map((click) => (
